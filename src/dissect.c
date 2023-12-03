b4d9dfcfe8bf1c1b5da9474280807435eeabb8bf
embeddedforallefa <veeresh.sharan@gmail.com>
2023-12-03 14:23:38 +0530

    dissect c program

---
+++ src/dissect.c
@@ -0,0 +1,7 @@
+/* A Simple C Program - for understanding */
+#include <stdio.h>                  // This is a preprocessor directive
+int main(void)                      // This identifies the function main()
+{                                   // This marks the beginning of main()
+    printf("This program is meant to dissect!");
+    return 0;                       // This returns control to the operating system
+}                                   // This marks the end of main()
