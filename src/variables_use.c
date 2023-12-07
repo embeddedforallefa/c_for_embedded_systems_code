1f78de609b72dbac9bb6435b1da6763088a09bd8
embeddedforallefa <veeresh.sharan@gmail.com>
2023-12-07 08:34:48 +0530

    data types and variables

---
+++ src/variables_use.c
@@ -0,0 +1,16 @@
+// Learn to use variables in C
+#include <stdio.h>
+int main()
+{
+    // Addition of two integers
+    int first_number; // Declare a variable called first_number
+    int second_number;
+    int result;
+
+    first_number = 10; // Store 10 in salary
+    second_number = 20;
+    result = first_number + second_number; // perform addition and store the value in result
+
+    printf("Addition: %d + %d = %d.\n", first_number, second_number, result);
+    return 0;
+}
