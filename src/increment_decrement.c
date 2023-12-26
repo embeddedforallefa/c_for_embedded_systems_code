64a0a2ad8bf66b7a8660440ef88151aee0ea4f34
embeddedforallefa <veeresh.sharan@gmail.com>
2023-12-26 23:40:26 +0530

    updated code

--- src/increment_decrement.c
+++ src/increment_decrement.c
@@ -6,26 +6,30 @@ int main() {
     int num1 = 5;
     // Post-increment: use the current value, then increment
     int result1 = num1++;
-    printf("Original value: %d\n", num1);
-    printf("Result after post-increment: %d\n", result1);
+    printf("------post-increment------\n");
+    printf("value of num1: %d\n", num1);
+    printf("value of Result1 : %d\n", result1);
 
     int num2 = 5;
     // Pre-increment: increment first, then use the updated value
     int result2 = ++num2;
-    printf("Original value: %d\n", num2);
-    printf("Result after pre-increment: %d\n", result2);
+    printf("------pre-increment------\n");
+    printf("value of num2: %d\n", num2);
+    printf("value of Result2 : %d\n", result2);
     
     int num3 = 8;
     // Post-decrement: use the current value, then decrement
     int result3 = num3--;
-    printf("Original value: %d\n", num3);
-    printf("Result after post-decrement: %d\n", result3);
+    printf("------post-decrement------\n");
+    printf("value of num3: %d\n", num3);
+    printf("value of Result3 : %d\n", result3);
 
     int num4 = 8;
     // Pre-decrement: decrement first, then use the updated value
     int result4 = --num4;
-    printf("Original value: %d\n", num4);
-    printf("Result after pre-decrement: %d\n", result4);
+    printf("------pre-decrement------\n");
+    printf("value of num4: %d\n", num4);
+    printf("value of Result4 : %d\n", result4);
 
     return 0;
 }
