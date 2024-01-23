e086c1b0df0757a8146c0dfda8ddca04dfe95eb4
embeddedforallefa <veeresh.sharan@gmail.com>
2024-01-24 00:46:55 +0530

    union

---
+++ src/union.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+// Define a union named 'Data'
+union Data {
+    int intValue;
+    float floatValue;
+    char stringValue[20];
+};
+
+int main() {
+    // Declare an instance of the union
+    union Data data;
+
+    // Assign values to the members
+    data.intValue = 42;
+    printf("intValue: %d\n", data.intValue);
+
+    data.floatValue = 3.14f;
+    printf("floatValue: %.2f\n", data.floatValue);
+
+    // The union shares the same memory space, so intValue is now overwritten by floatValue
+    printf("intValue after floatValue assignment: %d\n", data.intValue);
+
+    // Assign a string to the stringValue member
+    strcpy(data.stringValue, "Hello, Union!");
+    printf("stringValue: %s\n", data.stringValue);
+
+    // Accessing intValue after assigning a string will yield unpredictable results
+    printf("intValue after stringValue assignment: %d\n", data.intValue);
+
+    return 0;
+}
